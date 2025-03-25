2ef0d8405ba6b4c80a55787f5c5e424302d20aea
Sean <seantupps@gmail.com>
2025-03-25 00:39:03 -0400

    Add Render domain to ALLOWED_HOSTS

--- mental_math_website/settings.py
+++ mental_math_website/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mathweb-kuml.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
